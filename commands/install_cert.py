53f0dc1c6a9726d9730bb9e5116eef2206072c27
toancse <minhtoanaglx@gmail.com>
2024-08-29 10:02:59 +0700

    fix: deleted file 9a5ba575.0 and modified install_cert.py (add feature rm 9a5ba575.0 after install certificate)

--- commands/install_cert.py
+++ commands/install_cert.py
@@ -40,7 +40,9 @@ def install_certificate(args):
         # install via adb
         run_adb_command("push 9a5ba575.0 /data/local/tmp/")
         run_adb_command("shell su -c \"cp /data/local/tmp/9a5ba575.0 /data/misc/user/0/cacerts-added/\"")
+        run_command("rm 9a5ba575.0")
         inp = input(f"{ANSI.YELLOW}Please reboot the device to apply the changes. Type Y or N: {ANSI.RESET}")
+        run_adb_command()
         if inp == "Y":
             run_adb_command("reboot")
             print(f"{ANSI.GREEN}Certificate installed successfully!{ANSI.RESET}")
